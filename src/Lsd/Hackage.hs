b36dd72ddb1929026dd9ecbc15a2f7ba2d84e9fb
patrick brisbin <pbrisbin@gmail.com>
2021-12-16 10:13:12 -0500

    Fetch Hackage details via JSON, not RSS
    This was hard to find!

--- src/Lsd/Hackage.hs
+++ src/Lsd/Hackage.hs
@@ -5,17 +5,15 @@ module Lsd.Hackage
 
 import RIO
 
-import Conduit
+import Data.Aeson
 import Lsd.PackageName
 import Lsd.Version
 import Network.HTTP.Simple
+import Network.HTTP.Types.Header (hAccept)
 import Network.HTTP.Types.Status (status200)
+import qualified RIO.ByteString.Lazy as BSL
 import RIO.List (headMaybe)
 import RIO.Text (unpack)
-import qualified RIO.Text as T
-import Text.RSS.Conduit.Parse
-import Text.RSS.Types
-import Text.XML.Stream.Parse (def, parseLBS)
 
 newtype HackageDetails = HackageDetails
   { hdVersion :: Version
@@ -26,43 +24,49 @@ getHackageDetails
   => PackageName
   -> m (Maybe HackageDetails)
 getHackageDetails package = do
-
   req <-
     liftIO
     $ parseRequest
     $ unpack
     $ "https://hackage.haskell.org/package/"
     <> unPackageName package
-    <> ".rss"
+    <> "/preferred"
 
-  resp <- httpLBS req
+  -- We'll parse ourselves because it makes logging more convenient
+  resp <- httpLBS $ addRequestHeader hAccept "application/json" req
+
   let
-    mTitle = do
+    mBody = do
       guard $ getResponseStatus resp == status200
-
-      doc <-
-        join
-        $ runConduit
-        $ parseLBS def (getResponseBody resp)
-        .| rssDocument @NoExtensions
-
-      itemTitle <$> headMaybe (channelItems doc)
-
+      pure $ getResponseBody resp
     mVersion = do
-      title <- mTitle
-      suffix <- T.stripPrefix (unPackageName package <> "-")
-        $ T.takeWhile (/= ' ') title
-      parseVersion $ unpack suffix
+      body <- mBody
+      HackageVersionsPreferred {..} <- decode body
+      headMaybe hvpNormalVersion
 
   logDebug
     $ "Hackage details found for "
     <> display package
     <> ": "
-    <> "\n  RSS status: "
+    <> "\n  Status: "
     <> displayShow (getResponseStatus resp)
-    <> "\n  RSS item title: "
-    <> maybe "none" display mTitle
+    <> "\n  Response: "
+    <> maybe "none" (displayBytesUtf8 . BSL.toStrict) mBody
     <> "\n  Parsed version: "
     <> maybe "none" display mVersion
 
   pure $ HackageDetails <$> mVersion
+
+-- | <https://hackage.haskell.org/api#versions>
+data HackageVersionsPreferred = HackageVersionsPreferred
+  { hvpNormalVersion :: [Version]
+  , hvpUnpreferredVersion :: [Version]
+  , hvpDeprecatedVersion :: [Version]
+  }
+
+instance FromJSON HackageVersionsPreferred where
+  parseJSON = withObject "HackageVersionsPreferred" $ \o ->
+    HackageVersionsPreferred
+      <$> (o .:? "normal-version" .!= [])
+      <*> (o .:? "unpreferred-version" .!= [])
+      <*> (o .:? "deprecated-version" .!= [])
