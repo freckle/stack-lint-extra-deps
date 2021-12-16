3ab794439dc6ca1deb03f0ce4159a4ac2aee5489
patrick brisbin <pbrisbin@gmail.com>
2021-12-16 08:52:29 -0500

    Add Lsd.RunSpec
    This serves as a basic smoke-test that we're functioning. By limiting to
    Hackage checks and committing the cache directory, we can ensure the
    test is reliable and robust.

---
+++ test/Lsd/RunSpec.hs
@@ -0,0 +1,38 @@
+module Lsd.RunSpec
+  ( spec
+  ) where
+
+import RIO
+
+import Lsd.App
+import Lsd.Checks
+import Lsd.Options
+import Lsd.Report
+import Lsd.Run
+import Lsd.StackYaml
+import Test.Hspec (Spec, describe, example, it)
+import Test.Hspec.Expectations.Lifted
+
+spec :: Spec
+spec = do
+  describe "runLsd" $ do
+    it "finds 8 Hackage suggestions in the lts-18.18 example" $ example $ do
+      let opts = testExampleOptions "lts-18.18" HackageChecks
+      stackYaml <- loadStackYaml $ oPath opts
+      withApp opts $ \app -> runRIO app $ do
+        n <- runLsd opts stackYaml $ \_ _ -> pure ()
+        n `shouldBe` 8
+
+testExampleOptions :: String -> ChecksName -> Options
+testExampleOptions name checks = Options
+  { oResolver = Nothing
+  , oExcludes = []
+  , oChecks = checks
+  , oFormat = Detailed
+  , oNoExit = True
+  , oCacheDirectory = "test/examples/.cache"
+  , oNoCache = False
+  , oColor = ColorNever
+  , oVerbose = False
+  , oPath = "test/examples/" <> name <> ".yaml"
+  }
