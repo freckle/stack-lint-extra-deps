f9771e4a5e2a1da00182153bfba775845003549b
patrick brisbin <pbrisbin@gmail.com>
2022-05-17 10:31:45 -0400

    Support github short-hand for git-extra-deps
    The github key is short-hand,
    
        github: foo/bar
        => git: https://github.com/foo/bar
    
    So we should parse that too.
    
    With the updated test file, we see it works:
    
        [debug] Git details for https://github.com/freckle/yesod-routes-flow:
          Commits to HEAD: 17
          Versions by tags: 1.0 (-27 commits), 1.0.1 (-26 commits), 1.0.2 (-24 commits), 1.1 (-21 commits), 1.1.1 (-19 commits), 2.0 (-14 commits), 3.0.0.1 (15 commits), 3.0.0.2 (17 commits)
        [error] Replace https://github.com/freckle/yesod-routes-flow@2a9cd87
                ↳ Same-or-newer version (3.0.0.2) exists on Hackage
        [error] Replace https://github.com/freckle/yesod-routes-flow@2a9cd87
                ↳ There are newer commits (17) on the default branch

--- src/GitExtraDep.hs
+++ src/GitExtraDep.hs
@@ -18,8 +18,10 @@ data GitExtraDep = GitExtraDep
   deriving stock Show
 
 instance FromJSON GitExtraDep where
-  parseJSON = withObject "GitExtraDep"
-    $ \o -> GitExtraDep <$> o .: "git" <*> o .: "commit"
+  parseJSON = withObject "GitExtraDep" $ \o -> do
+    gh <- o .:? "github"
+    repo <- maybe (o .: "git") (pure . Repository . (ghBase <>)) gh
+    GitExtraDep repo <$> o .: "commit"
 
 instance Display GitExtraDep where
   display GitExtraDep {..} = display gedRepository <> "@" <> display gedCommit
@@ -30,7 +32,7 @@ newtype Repository = Repository
     deriving newtype (Show, Display, FromJSON)
 
 repositoryBase :: Repository -> Text
-repositoryBase = T.dropPrefix "https://github.com/" . unRepository
+repositoryBase = T.dropPrefix ghBase . unRepository
 
 repositoryBaseName :: Repository -> Text
 repositoryBaseName = T.drop 1 . T.dropWhile (/= '/') . repositoryBase
@@ -42,3 +44,6 @@ newtype CommitSHA = CommitSHA
 
 instance Display CommitSHA where
   display (CommitSHA x) = display $ T.take 7 x
+
+ghBase :: Text
+ghBase = "https://github.com/"
