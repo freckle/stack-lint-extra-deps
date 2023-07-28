6877b040fcde0be4524667c6a57de303d6bad7ca
patrick brisbin <pbrisbin@gmail.com>
2023-07-28 13:36:10 -0400

    Re-enable Spec

--- test/SLED/RunSpec.hs
+++ test/SLED/RunSpec.hs
@@ -4,36 +4,35 @@ module SLED.RunSpec
 
 import SLED.Prelude
 
--- import SLED.App
--- import SLED.Checks
--- import SLED.Options
--- import SLED.Report
--- import SLED.Run
--- import SLED.StackYaml
--- import Test.Hspec (Spec, describe, example, it)
--- import Test.Hspec.Expectations.Lifted
-import Test.Hspec
+import Blammo.Logging.Logger
+import SLED.App
+import SLED.Checks
+import SLED.Options
+import SLED.Report
+import SLED.Run
+import SLED.StackYaml
+import Test.Hspec (Spec, describe, example, it)
+import Test.Hspec.Expectations.Lifted
 
 spec :: Spec
 spec = do
-  pure ()
+  describe "runLsd" $ do
+    it "finds 10 Hackage suggestions in the lts-18.18 example" $ example $ do
+      let opts = testExampleOptions "lts-18.18" HackageChecks
+      app <- App opts <$> newTestLogger defaultLogSettings
+      stackYaml <- loadStackYaml $ oPath opts
+      flip runAppT app $ do
+        n <- runLsd opts stackYaml $ \_ -> pure ()
+        n `shouldBe` 10
 
--- describe "runLsd" $ do
---   it "finds 10 Hackage suggestions in the lts-18.18 example" $ example $ do
---     let opts = testExampleOptions "lts-18.18" HackageChecks
---     stackYaml <- loadStackYaml $ oPath opts
---     withApp opts $ \app -> runRIO app $ do
---       n <- runLsd opts stackYaml $ \_ -> pure ()
---       n `shouldBe` 10
-
--- testExampleOptions :: String -> ChecksName -> Options
--- testExampleOptions name checks =
--- Options
---   { oResolver = Nothing
---   , oExcludes = []
---   , oChecks = checks
---   , oFormat = Detailed
---   , oNoExit = True
---   , oPath = "test/examples/" <> name <> ".yaml"
---   , oFilter = Nothing
---   }
+testExampleOptions :: String -> ChecksName -> Options
+testExampleOptions name checks =
+  Options
+    { oResolver = Nothing
+    , oExcludes = []
+    , oChecks = checks
+    , oFormat = Detailed
+    , oNoExit = True
+    , oPath = "test/examples/" <> name <> ".yaml"
+    , oFilter = Nothing
+    }
