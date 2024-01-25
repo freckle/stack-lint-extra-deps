18b64f814e4654fc922fcfdfdaf9290a3eb8286b
patrick brisbin <pbrisbin@gmail.com>
2024-01-25 08:59:31 -0500

    Rewrite GitSpec

--- test/SLED/Checks/GitSpec.hs
+++ test/SLED/Checks/GitSpec.hs
@@ -16,100 +16,70 @@ import SLED.Version
 
 spec :: Spec
 spec = do
+  let runGitChecks mockGit ged =
+        runTestChecks mempty mempty mockGit lts1818 GitChecks
+          $ markAtZero (Git ged) "<input>"
+
   describe "checkGitVersion" $ do
     it "suggests if there are newer commits" $ do
       let
-        gitDep1 =
-          GitExtraDep
-            { repository = Repository "freckle/foo"
-            , commit = markAtZero (CommitSHA "xxxxx") "<input>"
-            }
-        gitDep2 =
-          GitExtraDep
-            { repository = Repository "freckle/foo"
-            , commit = markAtZero (CommitSHA "yyyyy") "<input>"
-            }
-        extraDep = markAtZero (Git gitDep1) "<input>"
-
+        repo = Repository "freckle/foo"
+        commitX = CommitSHA "xxxxx"
+        commitY = CommitSHA "yyyyy"
+        mcommitX = markAtZero commitX "<input>"
         mockGit =
           Just
             $ NE.fromList
             $ map
               (,Nothing)
-              [ markedItem gitDep2.commit
+              [ commitY
               , CommitSHA "abc456"
               , CommitSHA "def456"
               , CommitSHA "jkl789"
-              , markedItem gitDep1.commit
+              , commitX
               , CommitSHA "xyz012"
               ]
 
-      suggestions <-
-        runTestChecks
-          mempty
-          mempty
-          mockGit
-          lts1818
-          GitChecks
-          extraDep
-
-      suggestions
-        `shouldBe` [ Suggestion
-                      { action =
-                          ReplaceCommit (markAtZero (CommitSHA "xxxxx") "<input>") (CommitSHA "yyyyy")
-                      , reason = "There are 4 newer commits on the default branch"
-                      }
-                   ]
+      runGitChecks mockGit GitExtraDep {repository = repo, commit = mcommitX}
+        `shouldReturn` [ Suggestion
+                          { action = ReplaceCommit mcommitX commitY
+                          , reason = "There are 4 newer commits on the default branch"
+                          }
+                       ]
 
   describe "checkRedundantGit" $ do
     it "suggests if there are newer version-like tags" $ do
       let
-        gitDep1 =
-          GitExtraDep
-            { repository = Repository "freckle/foo"
-            , commit = markAtZero (CommitSHA "xxxxx") "<input>"
-            }
-        gitDep2 =
-          GitExtraDep
-            { repository = Repository "freckle/foo"
-            , commit = markAtZero (CommitSHA "yyyyy") "<input>"
-            }
-        extraDep = markAtZero (Git gitDep1) "<input>"
-
+        repo = Repository "freckle/foo"
+        commitX = CommitSHA "xxxxx"
+        commitY = CommitSHA "yyyyy"
+        mcommitX = markAtZero commitX "<input>"
+        ged = GitExtraDep {repository = repo, commit = mcommitX}
+        mged = markAtZero ged "<input>"
         mockGit :: Maybe (NonEmpty (CommitSHA, Maybe Text))
         mockGit =
           Just
             $ NE.fromList
-              [ (markedItem gitDep2.commit, Just "v1.0.2")
+              [ (commitY, Just "v1.0.2")
               , (CommitSHA "abc456", Nothing)
               , (CommitSHA "def456", Just "beta")
               , (CommitSHA "jkl789", Nothing)
-              , (markedItem gitDep1.commit, Just "v1.0.0")
+              , (commitX, Just "v1.0.0")
               ]
 
-      suggestions <-
-        runTestChecks
-          mempty
-          mempty
-          mockGit
-          lts1818
-          GitChecks
-          extraDep
-
-      suggestions
-        `shouldBe` [ Suggestion
-                      { action =
-                          ReplaceGitWithHackage (gitDep1 <$ extraDep)
-                            $ HackageExtraDep
-                              { package = PackageName "foo"
-                              , version = parseVersion "1.0.2"
-                              , checksum = Nothing
-                              }
-                      , reason = "Newer, version-like tag exists"
-                      }
-                   , Suggestion
-                      { action =
-                          ReplaceCommit (markAtZero (CommitSHA "xxxxx") "<input>") (CommitSHA "yyyyy")
-                      , reason = "There are 4 newer commits on the default branch"
-                      }
-                   ]
+      runGitChecks mockGit ged
+        `shouldReturn` [ Suggestion
+                          { action =
+                              ReplaceGitWithHackage mged
+                                $ HackageExtraDep
+                                  { package = PackageName "foo"
+                                  , version = parseVersion "1.0.2"
+                                  , checksum = Nothing
+                                  }
+                          , reason = "Newer, version-like tag exists"
+                          }
+                       , Suggestion
+                          { action = ReplaceCommit mcommitX commitY
+                          , reason = "There are 4 newer commits on the default branch"
+                          }
+                       ]
