19c6d715f364f089596c7a86c43eba0c72429e36
patrick brisbin <pbrisbin@gmail.com>
2024-02-15 10:25:14 -0500

    Parse Options out of pragma comments
    The contents of the linted path are "parsed" for comments of the
    following format:
    
    ```yaml
    \# @sled [option...]
    ```
    
    Where `option...` is a valid command-line for `stack-lint-extra-deps`.
    
    If successful, these options are merged into the CLI options (with CLI
    taking precedence).
    
    Lines without the `@sled` prefix are ignored. Lines with the prefix but
    content that doesn't parse as shell-words, or does parse but are invalid
    usage, are also ignored, but with warnings printed to `stderr`.
    
    While any options can be given this way (except `--path`, which will
    have no effect), the most likely use-case is lines like:
    
    ```yaml
    \# @sled --exclude this-dep --exclude '**/that-dep'
    ```
    
    Which allow you to centralize (and document) your reasons for excluding
    certain dependencies.

---
+++ src/SLED/Options/Pragma.hs
@@ -0,0 +1,36 @@
+module SLED.Options.Pragma
+  ( parsePragmaOptions
+  ) where
+
+import SLED.Prelude
+
+import qualified Data.ByteString.Char8 as BS8
+import Data.Char (isSpace)
+import qualified Data.Text as T
+import Options.Applicative (ParserInfo, ParserResult (..))
+import qualified Options.Applicative as Options
+import SLED.Options
+import qualified ShellWords
+
+parsePragmaOptions :: ByteString -> ([String], Options)
+parsePragmaOptions =
+  second fold . partitionEithers . mapMaybe parsePragma . BS8.lines
+
+parsePragma :: ByteString -> Maybe (Either String Options)
+parsePragma =
+  fmap (execParser optionsParserInfo <=< ShellWords.parse) . fromComment
+
+fromComment :: ByteString -> Maybe String
+fromComment bs =
+  unpack <$> do
+    c <- T.stripPrefix "# " $ decodeUtf8With lenientDecode bs
+    T.stripPrefix "@sled " $ T.dropWhile isSpace c
+
+execParser :: ParserInfo a -> [String] -> Either String a
+execParser p = handleParserResult . Options.execParserPure Options.defaultPrefs p
+
+handleParserResult :: ParserResult a -> Either String a
+handleParserResult = \case
+  Success a -> Right a
+  Failure x -> Left $ fst $ Options.renderFailure x "stack-lint-extra-deps"
+  CompletionInvoked {} -> Left "Unexpected completion invocation"
